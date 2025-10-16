a115a048622a8f0b1eada572c8f679bb4463a52a
Nico Welles <nico@welles.email>
2025-10-16 23:22:21 +0200

    Fix astro base path while in development

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: "https://stamm-phoenix.github.io",
+  base: "/nikolaus-astro",
   vite: {
     css: {
       preprocessorOptions: {}
