e8d1f2a6a9695b7ad809c16b9564c568786a4653
Nico Welles <nico@welles.email>
2025-10-16 23:19:53 +0200

    Add bootstrap and play around with astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,9 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: "https://stamm-phoenix.github.io",
-  base: "/nikolaus-astro"
+  vite: {
+    css: {
+      preprocessorOptions: {}
+    }
+  }
 });
