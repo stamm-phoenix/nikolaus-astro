ba7d2500d435b1f0abaf2d0cc5916abfe96a22bd
Nico Welles <nico@welles.email>
2025-10-16 21:57:02 +0200

    Add workflow to depoy to GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://stamm-phoenix.github.io",
+  base: "/nikolaus-astro"
+});
